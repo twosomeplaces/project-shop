24fe0e6442b0efff3527a490f472b648b5de7e4a
BC-Min <129809087+BC-Min@users.noreply.github.com>
2024-03-25 13:25:12 +0900

    orderpaymentkkk

--- src/main/java/project/shop/controller/MenuContoller.java
+++ src/main/java/project/shop/controller/MenuContoller.java
@@ -32,27 +32,42 @@ public class MenuContoller {
         return "menu/menu";
     }
 
-//    장바구니 호출
-    @GetMapping("/coffeeorder")
-    public String doOrder(Model model) {
-        List<OrderVo> orderlist = menusvc.doorderlist();
-        model.addAttribute("list", orderlist);
-
-        return "menu/coffeeorder";
-    }
-
-
     @GetMapping("/pay_seccess")
-    public String payment() {
+    public String payment(@RequestParam String sf) {
+        if(sf.equals("success")){
+            menusvc.dodeletecart();
+        }
         return "menu/paysuccess";
     }
 
+//    인덱스 -> 카트 이동
+    @GetMapping("coffeeorder")
+    public String order(Model model){
+
+        List<OrderVo> orderlist = menusvc.doorderlist();
+        model.addAttribute("list", orderlist);
+        return "menu/coffeeorder";
+    }
+
+//장바구니 리스트 중 한줄만 삭제
+    @GetMapping("deleteOne")
+    public String deleteOne(@RequestParam String orderid, @RequestParam String ordermenu){
+
+        menusvc.dodeleteone(orderid, ordermenu);
+
+        return "redirect:/coffeeorder";
+    }
+
+
 //    장바구니에 물건 담은 후 장바구니로 이동
     @GetMapping("/cart")
-    public String cartadd(@ModelAttribute OrderVo ordervo){
+    public String cartadd(@ModelAttribute OrderVo ordervo, Model model){
 
         menusvc.docartadd(ordervo);
-        return "redirect:/coffeeorder";
+
+        List<OrderVo> orderlist = menusvc.doorderlist();
+        model.addAttribute("list", orderlist);
+        return "menu/coffeeorder";
     }
 
 //    장바구니에 물건 담은 후 메뉴로 이동
--- src/main/java/project/shop/service/MenuService.java
+++ src/main/java/project/shop/service/MenuService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import org.springframework.web.bind.annotation.RequestParam;
 import project.shop.dao.MenuDao;
 import project.shop.vo.MenuVo;
 import project.shop.vo.OrderVo;
@@ -16,6 +17,7 @@ public class MenuService {
     MenuDao menuDao;
 
 
+//    메뉴리스트 조회
     public List<MenuVo> dolist(){
 
         List<MenuVo> list = menuDao.doList();
@@ -24,14 +26,29 @@ public class MenuService {
 
     };
 
+//    장바구니 담기
     public void docartadd(OrderVo ordervo) {
 
         menuDao.docartadd(ordervo);
     }
 
-    public List<OrderVo> doorderlist() {
 
-        List<OrderVo> list = menuDao.doorderlist();
+//장바구니 조회
+    public List<OrderVo> doorderlist() {
+        String id = "aa1";// 시큐리티에서 아이디 받아오면 그걸로 변경예정. 임시로 넣어둔거임.
+        List<OrderVo> list = menuDao.doorderlist(id);
+
         return list;
+    }
+
+//    장바구니전체삭제
+    public void dodeletecart() {
+        String id = "aa1"; // 시큐리티에서 아이디 받아오면 그걸로 변경예정. 임시로 넣어둔거임.
+        menuDao.dodeletecart(id);
+    }
+
+//    장바구니 한줄삭제
+    public void dodeleteone(String orderid, String ordermenu) {
+        menuDao.dodeleteone(orderid,ordermenu);
     }
 }
